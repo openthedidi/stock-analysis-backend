151317f1aaf3cb83d1544682cafe03288d58327d
openthedidi <openthedidi2004@gmail.com>
2025-07-05 15:41:56 +0900

    first commit

---
+++ src/main/java/com/stockanalysis/backend/StockAnalysisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAnalysisBackendApplication.class, args);
+    }
+
+}
